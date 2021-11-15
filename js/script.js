d81cbf1aacb6a20ef6a1bdd5630200a7f10b312e
Andrea <andreanuzzo@hotmail.it>
2021-11-15 14:56:30 +0100

    completed layout

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/* JSnack 1
+Il software deve chiedere per 5 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+Esegui questo programma in due versioni, con il for e con il while */
+
+
+
+
+
+
+
+
+
+
+/* JSnack 2
+Dare la possibilità di inserire due parole.
+Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.
+Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due. */
+
+/* JSnack 3
+Stampa le potenze di 2 fino a 1000. */
+
+
+
