de13dcdd0d4468e530adf3272476e95ef039ad0a
Andrea <andreanuzzo@hotmail.it>
2021-11-15 15:00:58 +0100

    implemented js whit addEventListener

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@ Il software deve chiedere per 5 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.
 Esegui questo programma in due versioni, con il for e con il while */
 
+const jSnack1 = document.querySelector('.first');
+
+jSnack1.addEventListener('click', function(){
+    alert('cliccato')
+});
+
+
 
 
 
@@ -16,9 +23,17 @@ Esegui questo programma in due versioni, con il for e con il while */
 Dare la possibilità di inserire due parole.
 Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.
 Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due. */
+const jSnack2 = document.querySelector('.second');
 
+jSnack2.    1('click', function(){
+    alert('cliccato2')
+});
 /* JSnack 3
 Stampa le potenze di 2 fino a 1000. */
 
+const jSnack3 = document.querySelector('.third');
 
+jSnack3.addEventListener('click', function(){
+    alert('cliccato3')
+});
 
