5e50d96de956be8ac8e83fbbe7955d5cb8b314f4
Andrea <andreanuzzo@hotmail.it>
2021-11-15 15:15:25 +0100

    completed JSnack 1 and 2

--- js/script.js
+++ js/script.js
@@ -6,16 +6,15 @@ Esegui questo programma in due versioni, con il for e con il while */
 const jSnack1 = document.querySelector('.first');
 
 jSnack1.addEventListener('click', function(){
-    alert('cliccato')
+    
+    let sum = 0;
+
+    for(let i = 0; i < 5; i++){
+        let numb = parseInt(prompt('Inserisci un numero'));
+        sum += numb;
+    }
+    alert(`La somma dei numeri inseriti è : ${sum}`)
 });
-
-
-
-
-
-
-
-
 
 
 
@@ -25,8 +24,17 @@ Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.
 Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due. */
 const jSnack2 = document.querySelector('.second');
 
-jSnack2.    1('click', function(){
-    alert('cliccato2')
+jSnack2.addEventListener('click', function(){
+    let firstWord = prompt('Inserisci la prima parola');
+    let secondWord = prompt('Inserisci la seconda parola');
+
+    if(firstWord.length == secondWord.length){
+        alert(`Le parole "${firstWord}" e "${secondWord}" hanno lo stesso numero di caratteri`);
+    } else if (firstWord.length > secondWord.length){
+        alert(`La parola "${firstWord}" possiede più caratteri`)
+    }else{
+        alert(`La parola "${secondWord}" possiede più caratteri`)
+    }
 });
 /* JSnack 3
 Stampa le potenze di 2 fino a 1000. */
